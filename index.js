e69ab3ba1a1796f9b268bf6e5bc2b8e1f588c83e
jameswhatcott <james@whatcott.com>
2024-07-25 21:29:49 -0500

    started some basics on the inquirer

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'What would you like to do?',
+      choices: ['View All Departments', 
+                'View All Roles', 
+                'View All Employees', 
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role']
+    },
+  
+  ])
+  .then((response) => {
+    generateLogo(response)
+
+  });
